7649b12dd0c542c17e632edc092e43267d1d6abb
Gizem Redif <gizemredif@gmail.com>
2025-02-23 17:18:04 +0300

    a

---
+++ TeduJam/Assets/Scenes/Episodes/Game End/ButtonInteraction.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class ButtonInteraction : MonoBehaviour
+{
+    private bool isPlayerNear = false; 
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            isPlayerNear = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            isPlayerNear = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.E)) 
+        {
+            SceneManager.LoadScene("Ended Successfully"); 
+        }
+    }
+}
